31ca048f9abdeaff5b248864556efbe96f19baf5
DartDivora <nsanft@sanft.com>
2016-11-27 13:00:15 -0600

    Added player.py
    Added player.py and made some minor changes.

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-import pygame   
-
-def test():
-    print("hello!")
+import pygame, player  
 
 pygame.init()
+
+#Display Settings and clock...
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Adventure game!")
 clock= pygame.time.Clock()
 
-isPlayerAlive = True
+playerAlive = True
 
-while isPlayerAlive:
+while player.isAlive():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            isPlayerAlive = False
+            player.setAlive(False)
         print(event)
-    pygame.display.update()
 
-test()    
+    #update
+    pygame.display.update()
+    player.update()
+    #FPS is set here!
+    clock.tick(60)
+
+pygame.quit()
+quit()
             
 
 
---
+++ player.py
@@ -0,0 +1,12 @@
+alive = True
+
+def isAlive():
+    #print("Player is currently: " + str(alive))
+    return alive
+
+def setAlive(boolean):
+    print("Setting alive to: " + str(boolean))
+    alive = boolean
+
+def update():
+    #nothing for now...
