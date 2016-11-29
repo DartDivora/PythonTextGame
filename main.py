8ea4f7a48e83da8c540d64c8bb3ae3e41448563a
DartDivora <nsanft@sanft.com>
2016-11-29 10:59:42 -0600

    Added Sprites, created basic displays

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ import pygame, player
 
 pygame.init()
 
+display_width = 800
+display_height = 600
+
+#colors are a tuple with RGB values
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
 #Display Settings and clock...
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption("Adventure game!")
 clock= pygame.time.Clock()
+playerImage = pygame.image.load('sprites/finger.png')
 
-playerAlive = True
+def updatePlayer(x,y):
+    gameDisplay.blit(playerImage,(x,y))
+x = (display_width * 0.45)
+y = (display_height * 0.8)
 
 while player.isAlive():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             player.setAlive(False)
-        print(event)
+        #print(event)
 
     #update
+    gameDisplay.fill(white)
+    updatePlayer(x,y)
     pygame.display.update()
     player.update()
     #FPS is set here!
--- player.py
+++ player.py
@@ -1,12 +1,26 @@
 alive = True
+playerX = 0
+playerY = 0
+
 
 def isAlive():
     #print("Player is currently: " + str(alive))
     return alive
 
+
 def setAlive(boolean):
     print("Setting alive to: " + str(boolean))
     alive = boolean
 
+
 def update():
     #nothing for now...
+    print("")
+
+
+def setPlayerX(x):
+    playerX = x
+
+
+def setPlayerY(y):
+    playerY = y   
