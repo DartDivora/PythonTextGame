60ab6aebb4631551dc6742b075954cbeda682a04
DartDivora <nsanft@sanft.com>
2017-01-07 22:52:43 -0600

    Added random word/description generators to utils.py, added words.json file
    Added some basic description generators to be used to show the player.
    Also added a words.json file to contain random adjectives/nouns to use
    for that.

--- utils.py
+++ utils.py
@@ -1,9 +1,34 @@
-import random
+import random, json
+
+debug = True
+
+def debugPrint(text):
+    if debug:
+        print(str(text))
+
+def randomWord(words):
+    random_word = words[random.randint(0, (len(words) - 1))]
+    debugPrint(random_word)
+    return random_word
+
+def randomDescription():
+    description = "you enter into a " + randomWord(adjectives) + " " + randomWord(rooms)
+    debugPrint(description)
+
 def Rand_Num_Gen(Min,Max):
     #Generates random number from a range of 2 numbers passed into the function
     Random_Number = random.randint(Min,Max)
-    print (Random_Number)
-    return Random_Number
+    debugPrint(Random_Number)
+    return Random_Number        
+
+with open('files/words.json') as wordsFile:    
+    wordsJSON = json.load(wordsFile)
+adjectives = wordsJSON["adjectives"].split("|")
+rooms = wordsJSON["rooms"].split("|")
+debugPrint(adjectives)
+debugPrint(rooms)
+
+
 
 
 
