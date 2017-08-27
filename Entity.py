c9227cd2952b332eb863f1e9b6882b4b47c30e35
DartDivora <nsanft@sanft.com>
2017-08-27 13:05:23 -0500

    New Entity class
    Created a new Entity class, which the Player class inherits from.

---
+++ Entity.py
@@ -0,0 +1,22 @@
+class Entity:
+
+    level = 1
+    exp = 50
+    HP = 20
+    attack = 4
+    defense = 0
+    money = 0
+
+    def printStats(self):
+        text = "HP: " + str(self.HP) + "\n"
+        text += "Attack: " + str(self.attack) + "\n"
+        text += "Defense: " + str(self.defense) + "\n"
+        text += "Money: " + str(self.money) + "\n"
+        print(text)
+        return text
+
+    def __init__(self):
+        print("Entity!")
+
+    def update(self):
+        pass
--- Player.py
+++ Player.py
@@ -1,26 +1,14 @@
 import TextAdventureStrings
+from Entity import Entity
 
 
-class Player:
-    level = 1
-    exp = 50
+class Player(Entity):
     currentLocation = "1"
-    HP = 20
-    attack = 4
-    defense = 0
-    money = 0
     isAlive = True
     isFighting = False
     currentText = TextAdventureStrings.npc["GameMaster"]
     previousText = []
 
-    def printStats(self):
-        text = "HP: " + str(self.HP) + "\n"
-        text += "Attack: " + str(self.attack) + "\n"
-        text += "Defense: " + str(self.defense) + "\n"
-        text += "Money: " + str(self.money) + "\n"
-        print(text)
-        return text
 
     def goBack(self):
         self.currentText = self.previousText.pop()
@@ -42,5 +30,6 @@ class Player:
         return False
 
     def update(self):
+
         if self.checkLevelUp():
             self.levelUp()
